213e1be8879cceb2b5658f96ffbc55c791989d0f
Timothe PIANEZZI <Timothe.PIANEZZI@diiage.org>
2023-04-25 13:40:38 +0200

    Ajout ConfigureCustomExceptionMiddleware (1.3.1)

---
+++ TCG.Common/Middlewares/MiddlewareException/ConfigureExcpetion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace TCG.Common.Middlewares.MiddlewareException
+{
+    public static class ConfigureException
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
