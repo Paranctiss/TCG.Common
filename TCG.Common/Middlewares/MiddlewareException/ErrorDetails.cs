da08240ac3f39769433356f0244f10b47146501d
Timothe PIANEZZI <Timothe.PIANEZZI@diiage.org>
2023-04-25 13:18:30 +0200

    Ajout du middleware de gestion d'exception (1.3.0)

---
+++ TCG.Common/Middlewares/MiddlewareException/ErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace TCG.Common.Middlewares.MiddlewareException
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ TCG.Common/Middlewares/MiddlewareException/ExceptionMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace TCG.Common.Middlewares.MiddlewareException
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        //private readonly ILogger _logger;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            //_logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (ValidationException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+            catch (NotFoundException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = validationException.Message
+                    }.ToString());
+                    break;
+                case NotFoundException notFoundException:
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = notFoundException.Message
+                    }.ToString());
+                    break;
+                default:
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "Internal Server Error."
+                    }.ToString());
+                    break;
+            }
+        }
+    }
+}
