e9ab36b2f38236932bd228504c66235b05049f7a
Timothe PIANEZZI <Timothe.PIANEZZI@diiage.org>
2023-06-12 11:05:10 +0200

    Ajout versioning => 1.3.20

---
+++ TCG.Common/Versioning/SwaggerConfig/ConfigureSwaggerOptions.cs
@@ -0,0 +1,45 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TCG.Common.Versioning.SwaggerConfig
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+        private readonly string _serviceName;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider, string serviceName)
+        {
+            _provider = provider;
+            _serviceName = serviceName;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        private OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = _serviceName + " Web API",
+                Version = description.ApiVersion.ToString(),
+                Description = "Web API du micro-service " + _serviceName,
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
