d68e9d47a0e82d68ae1e3143eda2e9bb97d9013b
Léa Arnould <lea.arnould@diiage.org>
2023-06-15 11:16:01 +0200

    userByToken

---
+++ TCG.Common/MassTransit/Messages/UserByToken.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCG.Common.MassTransit.Messages;
+
+public record UserByToken(string Token, CancellationToken cancellationToken);
