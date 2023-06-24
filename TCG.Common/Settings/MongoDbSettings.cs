503bd659fbe2fc44bd933db24ecc90af825cfd9d
Paranctiss <fli1@live.fr>
2023-06-24 15:30:10 +0200

    v.1.3.30 Changing mongo connection string

--- TCG.Common/Settings/MongoDbSettings.cs
+++ TCG.Common/Settings/MongoDbSettings.cs
@@ -6,6 +6,6 @@ public class MongoDbSettings
     public string Port { get; set; }
     public string User { get; set; }
     public string Password { get; set; }
-    public string ConnectionString => $"mongodb://{User}:{Password}@{Host}:{Port}/?ssl=false&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cscmongodb@";
+    public string ConnectionString => $"mongodb://{User}:{Password}@{Host}:{Port}/?ssl=false";
 
 }
